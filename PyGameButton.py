e60a7918989adee2d717a1ed6a05399c68957bca
kobemartin <kobe.martin1@gmail.com>
2020-01-25 12:40:04 -0500

    Flask intro

---
+++ PyGameButton.py
@@ -0,0 +1,11 @@
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
